987d7f1ad29212826e295b91e265397d9c8a8ef7
Aom Kham Phaung <akhamphaung@gmail.com>
2024-03-01 05:04:16 +0000

    wip: error handling

--- index.js
+++ index.js
@@ -39,28 +39,56 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// app.post('/api/shorturl', async(req, res) => {
+//   const input_url = req.body.url;
+//     try {
+//       let url = await Url.findOne({original_url: input_url});
+//       if(!url) {
+//         const shortUrl = await Url.countDocuments() + 1;
+//         url = new Url({original_url: input_url, short_url: shortUrl});
+//         await url.save();
+//       }
+//       res.status(200).json({original_url: url.original_url, short_url: url.short_url});
+//     }catch(err) {
+//       console.log(err);
+//       res.status(500).json({
+// 				error: err.message || 'Cannot get Users',
+// 			});
+//     } 
+// })
+
 app.post('/api/shorturl', async(req, res) => {
   const input_url = req.body.url;
-
-  dns.lookup(new URL(input_url).hostname, async(err) =>  {
-    if(err) {
-      return res.status(400).json({error: "invalid url"});
-    } 
-
-    try {
-      let url = await Url.findOne({original_url: input_url});
-      if(!url) {
-        const shortUrl = await Url.countDocuments() + 1;
-        url = new Url({original_url: input_url, short_url: shortUrl});
-        await url.save();
-      }
-      res.status(200).json({original_url: url.original_url, short_url: url.short_url});
-    }catch(err) {
+  dns.lookup(input_url.hostname, (err) => {
+    if(err && err.code === 'ENOTFOUND') {
       console.log(err);
-      res.status(500).json({
-				error: err.message || 'Cannot get Users',
-			});
+      res.status(500).json({error: "Invalid URL"});
+    } else {
+      Url.findOne({original_url: input_url}, (err, data) => {
+        if(err) {
+          console.log(err);
+        }
+        const shortUrl = Url.countDocuments() + 1;
+        const urlData = new Url({
+          original_url: input_url,
+          short_url: shortUrl
+        })
+      })
+
+      urlData.save((err) => {
+        res.status(500).json("Error saving to database");
+      })
+      return res.status(200).json({original_url: urlData.original_url, short_url: urlData.short_url});
     }
+  })
+})
+
+app.post('/api/shorturl/:shorturl', (req, res) => {
+  Url.findOne({short_url: req.params.shorturl}, (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    res.redirect(data.original_url);
   })
 })
 
